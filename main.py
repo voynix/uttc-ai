7993ffcefb241152c9d86b3d11d0a79cabd4c224
Adam Jacobson <voynix26@gmail.com>
2017-08-13 13:00:26 -0400

    more robust and permissive input handling

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 
 import ai
 
@@ -25,7 +26,9 @@ def run_game():
         legal_moves = current_board.get_moves()  # make sure this properly acknowledges wildcards
         while True:
             print 'Legal moves are: %r' % legal_moves
-            next_move = input('Enter a move as (sub_board, square): ')
+            next_move = get_move()
+            if next_move is None:
+                continue
             if next_move in legal_moves:
                 break
             else:
@@ -48,6 +51,42 @@ def check_victory(board):
         else:
             return False
     return True
+
+def get_move():
+    move = raw_input('Enter your move or \'help\': ')
+    if move == 'help':
+        print 'To play on the s-th sub-board in the p-th position, enter any of the following:'
+        print '\t(s, p)'
+        print '\ts-p'
+        print '\tsp'
+        print
+        print 'Sub-boards and positions are numbered as follows:'
+        print '\t+---+'
+        print '\t|012|'
+        print '\t|345|'
+        print '\t|678|'
+        print '\t+---+'
+        return None
+    else:
+        if len(move) == 2:  # sp
+            s, p = move[0], move[1]
+        elif len(move) == 3:  # s-p
+            s, p = move.split('-')
+        elif re.match(r'\(\d, ?\d\)', move) is not None:  # (s,p) and (s, p)
+            parts = move.split(',')
+            s = parts[0][-1]
+            p = parts[1][-2]
+        else:
+            print '\'%s\' is not parseable as a move' % move
+            return None
+        try:
+            sub_board = int(s)
+            position = int(p)
+        except ValueError:
+            print s, p
+            print '\'%s\' is not parseable as a move' % move
+            return None
+        return sub_board, position
 
 
 if __name__ == '__main__':
